e58229bd23a8e68c90c8a17c692b033642677284
sepiggy <jms1209@qq.com>
2022-01-31 00:25:42 +0800

    B4 p7 ok

--- ch_02springcloud/代码/springcloud_parent/springcloud_01eureka_server/src/main/java/com/baizhi/EurekaServerApplication.java
+++ ch_02springcloud/代码/springcloud_parent/springcloud_01eureka_server/src/main/java/com/baizhi/EurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer //开启当前应用是一个服务注册中心
+@EnableEurekaServer // 开启当前应用作为一个服务注册中心
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class,args);
