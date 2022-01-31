0087bba19b049fd15bf6fd9ffe7355c68b000477
sepiggy <jms1209@qq.com>
2022-01-31 08:03:20 +0800

    B4 p8 ok

--- ch_02springcloud/代码/springcloud_parent/springcloud_02eureka_client/src/main/java/com/baizhi/EurekaClientApplication.java
+++ ch_02springcloud/代码/springcloud_parent/springcloud_02eureka_client/src/main/java/com/baizhi/EurekaClientApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient //让 当前微服务作为一个eureka serve客户端 进行服务注册
+@EnableEurekaClient // 让当前微服务作为一个eureka-client，向eureka-server发起注册
 public class EurekaClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class,args);
+        SpringApplication.run(EurekaClientApplication.class, args);
     }
 }
