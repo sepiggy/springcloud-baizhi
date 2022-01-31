d5681b479bcb227acffc7299eab36da2d19b8d17
sepiggy <jms1209@qq.com>
2022-01-31 15:04:03 +0800

    B4 p12 ok

--- ch_02springcloud/代码/springcloud_parent/springcloud_03consulclient/src/main/java/com/baizhi/ConsulClientApplication.java
+++ ch_02springcloud/代码/springcloud_parent/springcloud_03consulclient/src/main/java/com/baizhi/ConsulClientApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication //代表这一个springboot入口应用
-@EnableDiscoveryClient  //作用:通用服务注册客户端注解 代表 consul client  zk client  nacos client
+@SpringBootApplication // 代表这是一个springboot入口应用
+@EnableDiscoveryClient  // SpringCloud提供的通用服务注册客户端注解，包括 consul-client, zk-client, nacos-client
 public class ConsulClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsulClientApplication.class,args);
+        SpringApplication.run(ConsulClientApplication.class, args);
     }
 }
