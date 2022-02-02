c0ce465d063c59b78099884be1d2a2bc6f64191c
sepiggy <jms1209@qq.com>
2022-02-02 10:40:41 +0800

    B4 p25 ok

--- ch_02springcloud/代码/springcloud_parent/springcloud_08hystrix/src/main/java/com/baizhi/HystrixApplication.java
+++ ch_02springcloud/代码/springcloud_parent/springcloud_08hystrix/src/main/java/com/baizhi/HystrixApplication.java
@@ -8,13 +8,11 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication  //代表springboot应用
-@EnableDiscoveryClient  //代表服务注册中心客户端   consul  client
-@EnableCircuitBreaker   //开启hystrix服务熔断
+@SpringBootApplication  // 代表springboot应用
+@EnableDiscoveryClient  // 代表服务注册中心客户端  consul-client
+@EnableCircuitBreaker   // 开启hystrix服务熔断
 public class HystrixApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HystrixApplication.class,args);
+        SpringApplication.run(HystrixApplication.class, args);
     }
-
-
 }
--- ch_02springcloud/代码/springcloud_parent/springcloud_08hystrix/src/main/java/com/baizhi/controller/DemoController.java
+++ ch_02springcloud/代码/springcloud_parent/springcloud_08hystrix/src/main/java/com/baizhi/controller/DemoController.java
@@ -4,35 +4,42 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class DemoController {
 
-
-
     @GetMapping("/demo1")
     @HystrixCommand(defaultFallback = "defaultFallBack")
-    public String demo1(){
+    public String demo1() {
         System.out.println("demo1 ok!!");
         return "demo1 ok !!!";
     }
 
+    /**
+     * 这里演示的是使用 Hystrix 完成服务提供方熔断
+     * 还可以使用 Hystrix 配合 OpenFeign 完成服务调用方异常快速响应
+     */
     @GetMapping("demo")
-    @HystrixCommand(defaultFallback = "defaultFallBack")//熔断之后处理 fallbackMethod 书写快速失败方法名
-    public String demo(Integer id){
-        System.out.println("demo ok !!!");
-        if(id<=0){
+    // 熔断之后处理
+    // fallbackMethod属性：该方法失败回调函数的方法名
+    // defaultFallBack属性：默认失败回调函数的方法名
+    @HystrixCommand(fallbackMethod = "demoFallBack")
+    public String demo(Integer id) { // ?id =
+        System.out.println(LocalDateTime.now());
+        if (id <= 0) {
             throw new RuntimeException("无效id!!!!");
         }
         return "demo ok !!!";
     }
 
-    //默认的处理方法
-    public String defaultFallBack(){
+    // 默认的处理方法
+    public String defaultFallBack() {
         return "网络连接失败,请重试!!!";
     }
 
-    //自己备选处理
-    public String demoFallBack(Integer id){
+    // 自己备选处理，备选方法与需要熔断功能方法的参数列表保持一致
+    public String demoFallBack(Integer id) {
         return "当前活动过于火爆,服务已经被熔断了!!!";
     }
 }
